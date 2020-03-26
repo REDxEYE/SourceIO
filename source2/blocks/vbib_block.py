4adf4dcc5a50ff2a301a084e24a613f411c277e4
REDxEYE <med45c@gmail.com>
2020-03-26 22:43:13 +0300

    WIP index/vertex data decompression

--- source2/blocks/vbib_block.py
+++ source2/blocks/vbib_block.py
@@ -1,3 +1,4 @@
+import struct
 from copy import copy
 from enum import IntEnum
 from typing import List
@@ -143,12 +144,12 @@ class DxgiFormat(IntEnum):
     V408 = 132,
 
 
-class CompressedVertexConstants:
-    vertex_header = 0xa0
-    vertex_block_size_bytes = 8192
-    vertex_block_max_size = 256
-    byte_group_size = 16
-    tail_max_size = 32
+index_header = 0xe0
+vertex_header = 0xa0
+vertex_block_size_bytes = 8192
+vertex_block_max_size = 256
+byte_group_size = 16
+tail_max_size = 32
 
 
 class VertexBuffer:
@@ -233,7 +234,7 @@ class VertexBuffer:
             return b << bits, data_var + is_same, encv if is_same else enc
 
         if bitslog2 == 0:
-            for k in range(CompressedVertexConstants.byte_group_size):
+            for k in range(byte_group_size):
                 destination[k] = 0
             return data
         elif bitslog2 == 1:
@@ -302,36 +303,36 @@ class VertexBuffer:
             return slice(data, data_var)
 
         elif bitslog2 == 3:
-            destination[:CompressedVertexConstants.byte_group_size] = data[0:CompressedVertexConstants.byte_group_size]
-            return slice(data, CompressedVertexConstants.byte_group_size)
+            destination[:byte_group_size] = data[0:byte_group_size]
+            return slice(data, byte_group_size)
         else:
             raise Exception("Unexpected bit length")
 
     def decode_bytes(self, data: np.ndarray, destination: np.ndarray):
-        assert destination.size % CompressedVertexConstants.byte_group_size == 0, "Expected data length to be a multiple of ByteGroupSize."
-        header_size = ((destination.size // CompressedVertexConstants.byte_group_size) + 3) // 4
+        assert destination.size % byte_group_size == 0, "Expected data length to be a multiple of ByteGroupSize."
+        header_size = ((destination.size // byte_group_size) + 3) // 4
         header = slice(data, 0)
         data: np.ndarray = slice(data, header_size)
-        for i in range(0, destination.size, CompressedVertexConstants.byte_group_size):
-            assert data.size > CompressedVertexConstants.tail_max_size, "Cannot decode"
-            header_offset = i // CompressedVertexConstants.byte_group_size
+        for i in range(0, destination.size, byte_group_size):
+            assert data.size > tail_max_size, "Cannot decode"
+            header_offset = i // byte_group_size
             bitslog2 = (header[header_offset // 4] >> ((header_offset % 4) * 2)) & 3
             data = self.decode_bytes_group(data, slice(destination, i), bitslog2)
         return data
 
     def get_vertex_block_size(self):
-        result = CompressedVertexConstants.vertex_block_size_bytes // self.vertex_size
-        result &= ~(CompressedVertexConstants.byte_group_size - 1)
-        return result if result < CompressedVertexConstants.vertex_block_max_size \
-            else CompressedVertexConstants.vertex_block_max_size
+        result = vertex_block_size_bytes // self.vertex_size
+        result &= ~(byte_group_size - 1)
+        return result if result < vertex_block_max_size \
+            else vertex_block_max_size
 
     def decode_vertex_block(self, data: np.ndarray, vertex_data: np.ndarray, vertex_count, last_vertex: np.ndarray):
-        assert vertex_count > 0 and vertex_count <= CompressedVertexConstants.vertex_block_max_size, \
+        assert vertex_count > 0 and vertex_count <= vertex_block_max_size, \
             "Expected vertexCount to be between 0 and VertexMaxBlockSize"
-        buffer = np.zeros((CompressedVertexConstants.vertex_block_max_size,), dtype=np.uint8)
-        transposed = np.zeros((CompressedVertexConstants.vertex_block_size_bytes,), dtype=np.uint8)
-        vertex_count_aligned = (vertex_count + CompressedVertexConstants.byte_group_size - 1) & ~(
-                CompressedVertexConstants.byte_group_size - 1)
+        buffer = np.zeros((vertex_block_max_size,), dtype=np.uint8)
+        transposed = np.zeros((vertex_block_size_bytes,), dtype=np.uint8)
+        vertex_count_aligned = (vertex_count + byte_group_size - 1) & ~(
+                byte_group_size - 1)
         for k in range(self.vertex_size):
             data = self.decode_bytes(data, slice(buffer, 0, vertex_count_aligned))
             vertex_offset = k
@@ -352,8 +353,8 @@ class VertexBuffer:
         assert len(buffer) > 1 + self.vertex_size, "Vertex buffer is too short."
         vertex_span = buffer.copy()
         header = vertex_span[0]
-        assert header == CompressedVertexConstants.vertex_header, \
-            f"Invalid vertex buffer header, expected {CompressedVertexConstants.vertex_header} but got {header}."
+        assert header == vertex_header, \
+            f"Invalid vertex buffer header, expected {vertex_header} but got {header}."
         vertex_span: np.ndarray = slice(vertex_span, 1)
         last_vertex: np.ndarray = slice(vertex_span, buffer.size - 1 - self.vertex_size, self.vertex_size)
         vertex_block_size = self.get_vertex_block_size()
@@ -427,21 +428,21 @@ class VertexAttribute:
 
 class IndexBuffer:
     def __init__(self):
-        self.count = 0
-        self.size = 0
+        self.index_count = 0
+        self.index_size = 0
         self.offset = 0
         self.unk1 = 0
         self.unk2 = 0
         self.total_size = 0
-        self.buffer = None  # type:ByteIO
+        self.buffer = ByteIO()  # type:ByteIO
         self.indexes = []
 
     def __repr__(self):
-        return '<IndexBuffer indexes:{} size:{}>'.format(self.count, self.size)
+        return '<IndexBuffer indexes:{} size:{}>'.format(self.index_count, self.index_size)
 
     def read(self, reader: ByteIO):
-        self.count = reader.read_uint32()
-        self.size = reader.read_uint32()
+        self.index_count = reader.read_uint32()
+        self.index_size = reader.read_uint32()
         self.unk1 = reader.read_uint32()
         self.unk2 = reader.read_uint32()
         entry = reader.tell()
@@ -449,12 +450,172 @@ class IndexBuffer:
         self.total_size = reader.read_uint32()
         with reader.save_current_pos():
             reader.seek(entry + self.offset)
-            assert self.total_size == self.size * self.count  # TODO: https://github.com/Silverlan/util_source2/blob/25cb557d19e48a34fe7dfd74d8e96f670a62f171/src/mesh_optimizer.cpp#L331
-            self.buffer = ByteIO(byte_object=reader.read_bytes(self.count * self.size))
+            if self.total_size != self.index_size * self.index_count:
+                self.buffer.write_bytes(self.decode_index_buffer(reader.read_bytes(self.total_size)))
+            else:
+                self.buffer.write_bytes(reader.read_bytes(self.index_count * self.index_size))
+            self.buffer.seek(0)
         self.read_buffer()
 
+    def decode_index_buffer(self, buffer: bytes):
+        buffer: np.ndarray = np.array(list(buffer), dtype=np.uint8)
+        assert self.index_count % 3 == 0, "Expected indexCount to be a multiple of 3."
+        assert self.index_size in [2, 4], "Expected indexSize to be either 2 or 4"
+        data_offset = 1 + (self.index_count // 3)
+        assert buffer.size > data_offset + 16, "Index buffer is too short."
+        assert buffer[0] == index_header, "Incorrect index buffer header."
+        vertex_fifo = np.array([], dtype=np.uint32)
+        vertex_fifo.resize((16,))
+        edge_fifo = np.array([], dtype=np.uint32)
+        edge_fifo.resize((16, 2))
+        edge_fifo_offset = 0
+        vertex_fifo_offset = 0
+
+        next = 0
+        last = 0
+
+        buffer_index = 1
+        data = slice(buffer, data_offset, buffer.size - 16 - data_offset)
+        codeaux_table = slice(buffer, buffer.size - 16)
+
+        destination = np.zeros((self.index_count * self.index_size))
+        ds = ByteIO(byte_object=bytes(data))
+        for i in range(0, self.index_count, 3):
+            code_tri = buffer[buffer_index]
+            buffer_index += 1
+
+            if code_tri < 0xF0:
+                fe = code_tri >> 4
+                ab = edge_fifo[((edge_fifo_offset - 1 - fe) & 15)]
+                fec = code_tri & 15
+                if fec != 15:
+                    c = next if fec == 0 else vertex_fifo[(vertex_fifo_offset - 1 - fec) & 15]
+                    fec0 = fec == 0
+                    next += fec0
+                    self.write_triangle(destination, i, ab[0], ab[1], c)
+                    vertex_fifo_offset = self.push_vertex_fifo(vertex_fifo, vertex_fifo_offset, c, fec0)
+                    edge_fifo_offset = self.push_edge_fifo(edge_fifo, edge_fifo_offset, c, ab[1])
+                    edge_fifo_offset = self.push_edge_fifo(edge_fifo, edge_fifo_offset, ab[0], c)
+                else:
+                    c = last = self.decode_index(ds, next, last)
+                    self.write_triangle(destination, i, ab[0], ab[1], c)
+                    vertex_fifo_offset = self.push_vertex_fifo(vertex_fifo, vertex_fifo_offset, c)
+
+                    edge_fifo_offset = self.push_edge_fifo(edge_fifo, edge_fifo_offset, c, ab[1])
+                    edge_fifo_offset = self.push_edge_fifo(edge_fifo, edge_fifo_offset, ab[0], c)
+            else:
+                if code_tri < 0xfe:
+                    codeaux = codeaux_table[code_tri & 15]
+                    feb = codeaux >> 4
+                    fec = codeaux & 15
+
+                    a = next
+                    next += 1
+
+                    b = next if feb == 0 else vertex_fifo[(vertex_fifo_offset - feb) & 15]
+                    feb0 = int(feb)
+                    next += feb0
+
+                    c = next if feb == 0 else vertex_fifo[(vertex_fifo_offset - feb) & 15]
+                    fec0 = int(fec)
+                    next += fec0
+
+                    self.write_triangle(destination, i, a, b, c)
+
+                    vertex_fifo_offset = self.push_vertex_fifo(vertex_fifo, vertex_fifo_offset, a)
+                    vertex_fifo_offset = self.push_vertex_fifo(vertex_fifo, vertex_fifo_offset, b, feb0 == 1)
+                    vertex_fifo_offset = self.push_vertex_fifo(vertex_fifo, vertex_fifo_offset, c, fec0 == 1)
+
+                    edge_fifo_offset = self.push_edge_fifo(edge_fifo, edge_fifo_offset, b, a)
+                    edge_fifo_offset = self.push_edge_fifo(edge_fifo, edge_fifo_offset, c, b)
+                    edge_fifo_offset = self.push_edge_fifo(edge_fifo, edge_fifo_offset, a, c)
+                else:
+                    codeaux = ds.read_uint8()
+                    fea = 0 if code_tri == 0xfe else 15
+                    feb = codeaux >> 4
+                    fec = codeaux & 15
+
+                    if fea == 0:
+                        a = next
+                        next += 1
+                    else:
+                        a = 0
+
+                    if fea == 0:
+                        b = next
+                        next += 1
+                    else:
+                        b = vertex_fifo[(vertex_fifo_offset - feb) & 15]
+
+                    if fec == 0:
+                        c = next
+                        next += 1
+                    else:
+                        c = vertex_fifo[(vertex_fifo_offset - fec) & 15]
+
+                    if fea == 15:
+                        last = a = self.decode_index(ds, next, last)
+                    if feb == 15:
+                        last = b = self.decode_index(ds, next, last)
+                    if fec == 15:
+                        last = c = self.decode_index(ds, next, last)
+
+                    self.write_triangle(destination, i, a, b, c)
+
+                    vertex_fifo_offset = self.push_vertex_fifo(vertex_fifo, vertex_fifo_offset, a)
+                    vertex_fifo_offset = self.push_vertex_fifo(vertex_fifo, vertex_fifo_offset, b,
+                                                               (feb == 0) or (feb == 15))
+                    vertex_fifo_offset = self.push_vertex_fifo(vertex_fifo, vertex_fifo_offset, c,
+                                                               (fec == 0) or (fec == 15))
+                    edge_fifo_offset = self.push_edge_fifo(edge_fifo, edge_fifo_offset, b, a)
+                    edge_fifo_offset = self.push_edge_fifo(edge_fifo, edge_fifo_offset, c, b)
+                    edge_fifo_offset = self.push_edge_fifo(edge_fifo, edge_fifo_offset, a, c)
+        assert ds.size() == ds.tell(), "we didn't read all data bytes and stopped before the boundary between data and codeaux table"
+
+        return bytes(destination)
+
+    def write_triangle(self, destination: np.ndarray, offset, a, b, c):
+        offset *= self.index_size
+        if self.index_size == 2:
+            ad = struct.pack('H', a)
+            bd = struct.pack('H', b)
+            cd = struct.pack('H', c)
+            destination[offset:offset + 6] = np.array(list(ad) + list(bd) + list(cd), dtype=np.uint8)
+        else:
+            ad = struct.pack('I', a)
+            bd = struct.pack('I', b)
+            cd = struct.pack('I', c)
+            destination[offset:offset + 12] = np.array(list(ad) + list(bd) + list(cd), dtype=np.uint8)
+
+    def push_vertex_fifo(self, fifo: np.ndarray, offset, v, cond=True):
+        fifo[offset] = v
+        return (offset + cond) & 15
+
+    def push_edge_fifo(self, fifo: np.ndarray, offset, a, b):
+        fifo[offset, :] = [a, b]
+        return (offset + 1) & 15
+
+    def decode_index(self, data: ByteIO, next, last):
+        v = self.decode_vbyte(data)
+        d = ((v >> 1) ^ -(v & 1))
+        return last + d
+
+    def decode_vbyte(self, data: ByteIO):
+        lead = data.read_uint8()
+        if lead < 128:
+            return lead
+        result = lead & 127
+        shift = 7
+        for i in range(4):
+            group = data.read_uint8()
+            result |= (group & 127) << shift
+            shift += 7
+            if group < 128:
+                break
+        return result
+
     def read_buffer(self):
-        for n in range(0, self.count, 3):
+        for n in range(0, self.index_count, 3):
             polygon = [self.buffer.read_uint16(), self.buffer.read_uint16(), self.buffer.read_uint16()]
             self.indexes.append(polygon)
 
