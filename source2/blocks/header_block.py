f8ac07177c551b5e9a66469ac2dd68d02aad4d91
RED_EYE <med45c@gmail.com>
2020-04-01 17:33:38 +0300

    meh

--- source2/blocks/header_block.py
+++ source2/blocks/header_block.py
@@ -6,16 +6,18 @@ class CompiledHeader:
     def __init__(self):
         super().__init__()
         self.file_size = 0
-        self.unk = 0
+        self.header_version = 0
+        self.resource_version = 0
         self.block_info_offset = 0
         self.block_count = 0
 
     def read(self, reader: ByteIO):
-        self.file_size = reader.read_int32()
-        self.unk = reader.read_int32()
-        # assert self.unk == 0x0000000c
-        self.block_info_offset = reader.read_int32()
-        self.block_count = reader.read_int32()
+        self.file_size = reader.read_uint32()
+        self.header_version = reader.read_uint16()
+        assert self.header_version == 0x0000000c
+        self.resource_version = reader.read_uint16()
+        self.block_info_offset = reader.read_uint32()
+        self.block_count = reader.read_uint32()
 
 
 class InfoBlock:
@@ -34,6 +36,6 @@ class InfoBlock:
     def read(self, reader: ByteIO):
         self.block_name = reader.read_fourcc()
         self.entry = reader.tell()
-        self.block_offset = reader.read_int32()
-        self.block_size = reader.read_int32()
+        self.block_offset = reader.read_uint32()
+        self.block_size = reader.read_uint32()
         self.absolute_offset = self.entry + self.block_offset
