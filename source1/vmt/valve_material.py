7653ffebf9c9efd3d2e068bf264d1a4502a78b1f
REDxEYE <med45c@gmail.com>
2020-12-21 02:59:34 +0300

    why source is so inconsistent?

--- source1/vmt/valve_material.py
+++ source1/vmt/valve_material.py
@@ -6,18 +6,20 @@ class VMT:
     def __init__(self, file_object):
         kv_parser = KVParser('VMT', file_object.read(-1).decode().replace('`', ''))
         self.shader, self.material_data = kv_parser.parse()
+        self.shader = self.shader.lower()
 
     def get_param(self, name, default):
         return self.material_data.get(name, default)
 
     def parse(self):
         content_manager = ContentManager()
-        if self.shader.lower() == 'patch':
+        if self.shader == 'patch':
             original_material = content_manager.find_file(self.material_data['include'])
             if original_material:
                 kv_parser = KVParser('VMT', original_material.read(-1).decode())
                 old_data = self.material_data
                 self.shader, self.material_data = kv_parser.parse()
+                self.shader = self.shader.lower()
                 if 'insert' in old_data:
                     patch_data = old_data['insert']
                     self.material_data.update(patch_data)
