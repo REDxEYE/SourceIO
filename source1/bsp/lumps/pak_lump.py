3863c465b6f5ba5b6505149d7d84a48542e3c95e
REDxEYE <med45c@gmail.com>
2020-12-21 03:00:50 +0300

    stupid valve, why are they storing names in one case in map, and in other in pakfile?

--- source1/bsp/lumps/pak_lump.py
+++ source1/bsp/lumps/pak_lump.py
@@ -11,10 +11,12 @@ class PakLump(Lump):
     def __init__(self, bsp):
         super().__init__(bsp)
         self.zip_file: zipfile.ZipFile = None
+        self._cache = {}
 
     def parse(self):
         zip_data = BytesIO(self.reader.read_bytes(self._lump.size))
         self.zip_file = zipfile.ZipFile(zip_data)
+        self._cache = {a.lower(): a for a in self.zip_file.NameToInfo}
         return self
 
     def find_file(self, filepath: str, additional_dir=None, extension=None):
@@ -25,10 +27,11 @@ class PakLump(Lump):
             new_filepath = Path(additional_dir, new_filepath)
         if extension:
             new_filepath = new_filepath.with_suffix(extension)
-        if str(new_filepath.as_posix()) in self.zip_file.NameToInfo:
-            return self.zip_file.open(str(new_filepath.as_posix()), 'r')
-        else:
-            return None
+        new_filepath = str(new_filepath.as_posix()).lower()
+        new_filepath = self._cache.get(new_filepath, None)
+        if new_filepath is not None:
+            return BytesIO(self.zip_file.open(new_filepath, 'r').read())
+        return None
 
     def find_texture(self, filepath):
         return self.find_file(filepath, 'materials', extension='.vtf')
