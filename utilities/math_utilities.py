3af68c1e015332201aba4484d8f31b518f98812f
REDxEYE <med45c@gmail.com>
2021-01-05 22:25:57 +0300

    add goldsrc math utilities

--- utilities/math_utilities.py
+++ utilities/math_utilities.py
@@ -7,6 +7,57 @@ import numpy as np
 def clamp_value(value, min_value=0.0, max_value=1.0):
     return min(max_value, max(value, min_value))
 
+
+def angle_matrix(pitch, yaw, roll):
+    sy = math.sin(yaw)
+    cy = math.cos(yaw)
+    sp = math.sin(pitch)
+    cp = math.cos(pitch)
+    sr = math.sin(roll)
+    cr = math.cos(roll)
+
+    matrix = np.zeros((3, 4))
+
+    matrix[0][0] = cp * cy
+    matrix[1][0] = cp * sy
+    matrix[2][0] = -sp
+    matrix[0][1] = sr * sp * cy + cr * -sy
+    matrix[1][1] = sr * sp * sy + cr * cy
+    matrix[2][1] = sr * cp
+    matrix[0][2] = (cr * sp * cy + -sr * -sy)
+    matrix[1][2] = (cr * sp * sy + -sr * cy)
+    matrix[2][2] = cr * cp
+    matrix[0][3] = 0
+    matrix[1][3] = 0
+    matrix[2][3] = 0
+    return matrix
+
+
+def r_concat_transforms(in1: np.ndarray, in2: np.ndarray):
+    out = np.zeros((3, 4))
+    out[0][0] = in1[0][0] * in2[0][0] + in1[0][1] * in2[1][0] + in1[0][2] * in2[2][0]
+    out[0][1] = in1[0][0] * in2[0][1] + in1[0][1] * in2[1][1] + in1[0][2] * in2[2][1]
+    out[0][2] = in1[0][0] * in2[0][2] + in1[0][1] * in2[1][2] + in1[0][2] * in2[2][2]
+    out[0][3] = in1[0][0] * in2[0][3] + in1[0][1] * in2[1][3] + in1[0][2] * in2[2][3] + in1[0][3]
+    out[1][0] = in1[1][0] * in2[0][0] + in1[1][1] * in2[1][0] + in1[1][2] * in2[2][0]
+    out[1][1] = in1[1][0] * in2[0][1] + in1[1][1] * in2[1][1] + in1[1][2] * in2[2][1]
+    out[1][2] = in1[1][0] * in2[0][2] + in1[1][1] * in2[1][2] + in1[1][2] * in2[2][2]
+    out[1][3] = in1[1][0] * in2[0][3] + in1[1][1] * in2[1][3] + in1[1][2] * in2[2][3] + in1[1][3]
+    out[2][0] = in1[2][0] * in2[0][0] + in1[2][1] * in2[1][0] + in1[2][2] * in2[2][0]
+    out[2][1] = in1[2][0] * in2[0][1] + in1[2][1] * in2[1][1] + in1[2][2] * in2[2][1]
+    out[2][2] = in1[2][0] * in2[0][2] + in1[2][1] * in2[1][2] + in1[2][2] * in2[2][2]
+    out[2][3] = in1[2][0] * in2[0][3] + in1[2][1] * in2[1][3] + in1[2][2] * in2[2][3] + in1[2][3]
+    return out
+
+
+def vector_transform(vec, mat):
+    out = np.zeros((3,))
+    out[0] = np.dot(vec, mat[0][:3]) + mat[0][3]
+    out[1] = np.dot(vec, mat[1][:3]) + mat[1][3]
+    out[2] = np.dot(vec, mat[2][:3]) + mat[2][3]
+    return out
+
+
 def convert_rotation_matrix_to_degrees(m0, m1, m2, m3, m4, m5, m8):
     angle_y = -math.asin(round(m2, 6))
     c = math.cos(angle_y)
