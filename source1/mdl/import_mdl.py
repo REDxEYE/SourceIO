e676a8ebec1f94490b3ff7aafae12d22e549465d
REDxEYE <med45c@gmail.com>
2021-03-11 14:23:36 +0300

    fix wrong materials indices

--- source1/mdl/import_mdl.py
+++ source1/mdl/import_mdl.py
@@ -189,11 +189,12 @@ def import_model(mdl_file: BinaryIO, vvd_file: BinaryIO, vtx_file: BinaryIO, sca
             mesh_data.normals_split_custom_set_from_vertices(vertices['normal'])
             mesh_data.use_auto_smooth = True
 
+            material_remapper = np.zeros((material_indices_array.max()+1,), dtype=np.uint32)
             for mat_id in np.unique(material_indices_array):
                 mat_name = mdl.materials[mat_id].name
-                get_material(mat_name[-63:], mesh_obj)
+                material_remapper[mat_id] = get_material(mat_name[-63:], mesh_obj)
 
-            mesh_data.polygons.foreach_set('material_index', material_indices_array[::-1].tolist())
+            mesh_data.polygons.foreach_set('material_index', material_remapper[material_indices_array[::-1]].tolist())
 
             mesh_data.uv_layers.new()
             uv_data = mesh_data.uv_layers[0].data
@@ -299,7 +300,7 @@ def import_materials(mdl):
     content_manager = ContentManager()
     for material in mdl.materials:
         if bpy.data.materials.get(material.name[-63:], False):
-            if bpy.data.materials[material.name[-63:]].get('source1_loaded',False):
+            if bpy.data.materials[material.name[-63:]].get('source1_loaded', False):
                 logger.info(f'Skipping loading of {material.name[-63:]} as it already loaded')
                 continue
         material_path = None
