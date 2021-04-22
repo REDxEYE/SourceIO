a2b82033dba5c704c8250498fba6d326368c8437
REDxEYE <med45c@gmail.com>
2021-04-22 23:29:36 +0300

    add decalmodulate shader

--- bpy_utilities/material_loader/material_loader.py
+++ bpy_utilities/material_loader/material_loader.py
@@ -11,10 +11,21 @@ from .shaders.goldsrc_shader_base import GoldSrcShaderBase
 from .shaders.source1_shader_base import Source1ShaderBase
 from .shaders.source2_shader_base import Source2ShaderBase
 
-from .shaders.source1_shaders import eyerefract, cable, unlit_generic, lightmap_generic, vertexlit_generic, \
-    worldvertextransition, unlittwotexture, lightmapped_4wayblend
+from .shaders.source1_shaders import (eyerefract,
+                                      cable,
+                                      unlit_generic,
+                                      lightmap_generic,
+                                      vertexlit_generic,
+                                      worldvertextransition,
+                                      unlittwotexture,
+                                      lightmapped_4wayblend,
+                                      decalmodulate)
 from .shaders.goldsrc_shaders import goldsrc_shader
-from .shaders.source2_shaders import vr_complex, vr_skin, vr_eyeball, vr_simple, vr_glass, hero
+from .shaders.source2_shaders import (vr_complex,
+                                      vr_skin,
+                                      vr_eyeball,
+                                      vr_simple, vr_glass, hero
+                                      )
 
 log_manager = BPYLoggingManager()
 logger = log_manager.get_logger('material_loader')
