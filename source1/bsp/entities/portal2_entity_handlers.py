0655aa6647c88ed4181da6891b5d16fdc87b34cf
REDxEYE <med45c@gmail.com>
2021-06-06 20:32:25 +0300

    Entity fix

--- source1/bsp/entities/portal2_entity_handlers.py
+++ source1/bsp/entities/portal2_entity_handlers.py
@@ -15,84 +15,31 @@ class Portal2EntityHandler(PortalEntityHandler):
     pointlight_power_multiplier = 1000
 
     def handle_prop_weighted_cube(self, entity: prop_weighted_cube, entity_raw: dict):
-        obj = self._create_empty(self._get_entity_name(entity))
-        properties = {'prop_path': entity.model,
-                      'type': entity.class_name,
-                      'scale': self.scale,
-                      'entity': entity_raw,
-                      'skin': entity.skin}
-
-        self._set_rotation(obj, entity.angles)
-        self._set_location_and_scale(obj, entity.origin)
-
-        self._set_entity_data(obj, properties)
+        obj = self._handle_entity_with_model(entity, entity_raw)
         self._put_into_collection('prop_weighted_cube', obj, 'props')
 
     def handle_prop_testchamber_door(self, entity: prop_testchamber_door, entity_raw: dict):
-        obj = self._create_empty(self._get_entity_name(entity))
-        properties = {'prop_path': entity.model,
-                      'type': entity.class_name,
-                      'scale': self.scale,
-                      'entity': entity_raw}
-
-        self._set_rotation(obj, entity.angles)
-        self._set_location_and_scale(obj, entity.origin)
-        self._set_entity_data(obj, properties)
+        obj = self._handle_entity_with_model(entity, entity_raw)
         self._put_into_collection('prop_testchamber_door', obj, 'props')
 
     def handle_prop_floor_button(self, entity: prop_floor_button, entity_raw: dict):
-        obj = self._create_empty(self._get_entity_name(entity))
-        properties = {'prop_path': entity.model,
-                      'type': entity.class_name,
-                      'scale': self.scale,
-                      'entity': entity_raw}
-        self._set_location_and_scale(obj, entity.origin)
-        self._set_rotation(obj, entity.angles)
-        self._set_entity_data(obj, properties)
+        obj = self._handle_entity_with_model(entity, entity_raw)
         self._put_into_collection('prop_floor_button', obj, 'props')
 
     def handle_prop_floor_ball_button(self, entity: prop_floor_ball_button, entity_raw: dict):
-        obj = self._create_empty(self._get_entity_name(entity))
-        properties = {'prop_path': entity.model,
-                      'type': entity.class_name,
-                      'scale': self.scale,
-                      'entity': entity_raw}
-        self._set_location_and_scale(obj, entity.origin)
-        self._set_rotation(obj, entity.angles)
-        self._set_entity_data(obj, properties)
+        obj = self._handle_entity_with_model(entity, entity_raw)
         self._put_into_collection('prop_floor_ball_button', obj, 'props')
 
     def handle_prop_floor_cube_button(self, entity: prop_floor_cube_button, entity_raw: dict):
-        obj = self._create_empty(self._get_entity_name(entity))
-        properties = {'prop_path': entity.model,
-                      'type': entity.class_name,
-                      'scale': self.scale,
-                      'entity': entity_raw}
-        self._set_location_and_scale(obj, entity.origin)
-        self._set_rotation(obj, entity.angles)
-        self._set_entity_data(obj, properties)
+        obj = self._handle_entity_with_model(entity, entity_raw)
         self._put_into_collection('prop_floor_cube_button', obj, 'props')
 
     def handle_prop_under_floor_button(self, entity: prop_under_floor_button, entity_raw: dict):
-        obj = self._create_empty(self._get_entity_name(entity))
-        properties = {'prop_path': entity.model,
-                      'type': entity.class_name,
-                      'scale': self.scale,
-                      'entity': entity_raw}
-        self._set_location_and_scale(obj, entity.origin)
-        self._set_rotation(obj, entity.angles)
-        self._set_entity_data(obj, properties)
+        obj = self._handle_entity_with_model(entity, entity_raw)
         self._put_into_collection('prop_under_floor_button', obj, 'props')
 
     def handle_prop_tractor_beam(self, entity: prop_tractor_beam, entity_raw: dict):
-        obj = self._create_empty(self._get_entity_name(entity))
-        properties = {'prop_path': entity.model,
-                      'type': entity.class_name,
-                      'scale': self.scale,
-                      'entity': entity_raw}
-        self._set_location_and_scale(obj, parse_float_vector(entity_raw.get('origin', '0 0 0')))
-        self._set_rotation(obj, parse_float_vector(entity_raw.get('angles', '0 0 0')))
-        self._set_entity_data(obj, properties)
+        obj = self._handle_entity_with_model(entity, entity_raw)
         self._put_into_collection('prop_tractor_beam', obj, 'props')
 
     def handle_logic_playmovie(self, entity: logic_playmovie, entity_raw: dict):
