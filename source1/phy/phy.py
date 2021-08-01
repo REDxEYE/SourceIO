8d1749c2fad1328573d41a68c3fd09181256cad6
REDxEYE <med45c@gmail.com>
2021-08-01 20:14:37 +0300

    Phy parser: Initial implementation

--- source1/phy/phy.py
+++ source1/phy/phy.py
@@ -1,26 +1,30 @@
 from typing import List
 
+from .structs.colliders import ColliderHeader, ColliderType, CompactSurfaceHeader, MoppHeader
 from .structs.header import Header
-from .structs.surface import CompactSurface
 
 from ...source_shared.base import Base
-from ...utilities.byte_io_mdl  import ByteIO
+from ...utilities.byte_io_mdl import ByteIO
 
 
 class Phy(Base):
     def __init__(self, filepath):
         self.reader = ByteIO(filepath)
         self.header = Header()
-        self.solids = []  # type:List[CompactSurface]
+        self.solids = []  # type:List[ColliderHeader]
         self.kv = ''
 
     def read(self):
-        self.header.read(self.reader)
-        self.reader.seek(self.header.header_size)
+        reader = self.reader
+        self.header.read(reader)
+        reader.seek(self.header.size)
         for _ in range(self.header.solid_count):
-            solid = CompactSurface()
-            solid.read(self.reader)
+            size = reader.read_uint32()
+            solid = ColliderHeader.peek(reader)
+            if solid.id == 'VPHY':
+                if solid.model_type == ColliderType.COLLIDE_POLY:
+                    solid = CompactSurfaceHeader()
+                elif solid.model_type == ColliderType.COLLIDE_MOPP:
+                    solid = MoppHeader()
+            solid.read(reader)
             self.solids.append(solid)
-        # kv = self.reader.read_ascii_string().replace('}', '\n}\n').replace('{', '\n{\n')
-        # kv_wrapped = 'phy_data \n{\n' + kv + '\n}'
-        # self.kv = KeyValueFile(None, string_buffer=kv_wrapped.split('\n'))
