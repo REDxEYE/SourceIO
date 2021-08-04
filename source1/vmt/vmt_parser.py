e4b21ba50f5b207e57585e1d5d07fc51fd8dbae0
REDxEYE <med45c@gmail.com>
2021-08-05 00:09:05 +0300

    VMT parser: Added preprocess step to try to correct invalid vmt files

--- source1/vmt/vmt_parser.py
+++ source1/vmt/vmt_parser.py
@@ -6,6 +6,36 @@ from ...utilities.keyvalues import KVParser
 from ...bpy_utilities.logging import BPYLoggingManager
 
 
+def _pre_process_vmt(vmt: str):
+    """Original code by https://github.com/myuce"""
+    result = ""
+    lines = vmt.replace("\t", " ").replace("\\", "/").replace(".vtf", "").split("\n")
+    for line in lines:
+        res2 = ""
+        line = line.replace('"', " ").strip().lower()
+        if line.startswith("{") and line != "{":
+            line = line[1:]
+            result += "{\n"
+        if line.endswith("}") and line != "}" and "{" not in line:
+            line = line[:-1]
+            res2 = "}\n"
+        if len(line) == 0 or line.startswith("/"):
+            continue
+        line = " ".join(line.split())
+        tok = line.split()
+        if len(tok) == 1:
+            result += line + "\n"
+            continue
+        key = tok[0]
+        value = " ".join(tok[1:])
+        if value == "{":
+            line = key + "\n{"
+        else:
+            line = f'"{key}" "{value}"'
+        result += line + "\n" + res2
+    return result
+
+
 class VMTParser:
     def __init__(self, file_or_string: Union[IO[str], IO[bytes], str]):
         self.logger = BPYLoggingManager().get_logger('vmt_parser')
@@ -20,12 +50,10 @@ class VMTParser:
             self._buffer = file_or_string.read().decode('latin', errors='replace')
         else:
             raise ValueError(f'Unknown input value type {type(file_or_string)}')
-
-        # if not self._buffer.strip().endswith('}'):
-        #     self.logger.debug(f'VMT has has missing closing "}}"')
-        #     self._buffer += '\n}'
-
-        self._parser = KVParser('<input>', self._buffer, single_value=True)
+        try:
+            self._parser = KVParser('<input>', _pre_process_vmt(self._buffer), single_value=True)
+        except Exception as ex:
+            self._parser = KVParser('<input>', self._buffer, single_value=True)
         self.header, self._raw_data = self._parser.parse()
 
     def get_vector(self, name, default=(0, 0, 0)):
