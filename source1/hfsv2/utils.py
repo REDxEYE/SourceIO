ad7ca54497cf7b4e42f99940b5fa1c9673abbd3f
REDxEYE <med45c@gmail.com>
2021-09-15 21:28:57 +0300

    Source1: HFS V1-V2 support.

---
+++ source1/hfsv2/utils.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+def calculate_header_offset(filename: str):
+    offset = 0
+    for char in filename.lower():
+        offset += ord(char)
+    return offset % 312 + 30
+
+
+def calculate_entry_table_offset(filename: str):
+    offset = 0
+    for char in filename.lower():
+        offset += ord(char) * 3
+    return offset % 212 + 33
