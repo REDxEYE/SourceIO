5720ef525d69740dddf0e3520719f60629f68f79
REDxEYE <med45c@gmail.com>
2021-09-17 17:19:21 +0300

    BSP: Added missing vindictus_entity_handler

---
+++ source1/bsp/entities/vindictus_entity_handler.py
@@ -0,0 +1,21 @@
+import math
+
+from mathutils import Euler, Vector
+import bpy
+from .base_entity_handler import BaseEntityHandler
+from .halflife2_entity_classes import *
+from .halflife2_entity_handler import HalfLifeEntityHandler
+
+local_entity_lookup_table = HalfLifeEntityHandler.entity_lookup_table.copy()
+local_entity_lookup_table.update(entity_class_handle)
+
+
+class VindictusEntityHandler(BaseEntityHandler):
+    entity_lookup_table = local_entity_lookup_table
+
+    def _handle_entity_with_model(self, entity, entity_raw: dict):
+        obj = super()._handle_entity_with_model(entity, entity_raw)
+        if 'renderscale' in entity_raw:
+            scale = parse_float_vector(entity_raw['renderscale'])
+            obj.scale *= Vector(scale)
+        return obj
