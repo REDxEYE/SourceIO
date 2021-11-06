ef6dcb57fab8edc5d9d26a0f70bfa420727721ce
REDxEYE <med45c@gmail.com>
2021-11-06 19:03:00 +0300

    Added support for env_portal_laser

--- blender_bindings/source1/bsp/entities/portal2_entity_handlers.py
+++ blender_bindings/source1/bsp/entities/portal2_entity_handlers.py
@@ -46,6 +46,10 @@ class Portal2EntityHandler(PortalEntityHandler):
         obj = self._handle_entity_with_model(entity, entity_raw)
         self._put_into_collection('prop_laser_catcher', obj, 'props')
 
+    def handle_env_portal_laser(self, entity: env_portal_laser, entity_raw: dict):
+        obj = self._handle_entity_with_model(entity, entity_raw)
+        self._put_into_collection('env_portal_laser', obj, 'props')
+
     def handle_logic_playmovie(self, entity: logic_playmovie, entity_raw: dict):
         obj = bpy.data.objects.new(self._get_entity_name(entity), None)
         self._set_location(obj, entity.origin)
