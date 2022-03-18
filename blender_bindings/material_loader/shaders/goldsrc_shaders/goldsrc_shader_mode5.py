e64f9e0b92931661f4245158b8a45a5b3b3a08ff
Irmitya <10817631+Irmitya@users.noreply.github.com>
2022-03-18 12:00:24 -0500

    Set $basetexture node as active node This will set it to be displayed in Textured shading

--- blender_bindings/material_loader/shaders/goldsrc_shaders/goldsrc_shader_mode5.py
+++ blender_bindings/material_loader/shaders/goldsrc_shaders/goldsrc_shader_mode5.py
@@ -15,6 +15,7 @@ class GoldSrcShaderMode5(GoldSrcShaderBase):
         basetexture = self.load_texture(material_name)
         basetexture_node = self.create_node(Nodes.ShaderNodeTexImage, '$basetexture')
         basetexture_node.image = basetexture
+        basetexture_node.id_data.nodes.active = basetexture_node
 
         if rad_info is not None:
             self._emit_surface(basetexture_node, rad_info)
