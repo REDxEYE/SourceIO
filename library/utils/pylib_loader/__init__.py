cb5758f31e38bf557e5788bf8f8ac7d10f4f190b
REDxEYE <med45c@gmail.com>
2022-06-30 22:34:13 +0300

    Import fix

--- library/utils/pylib_loader/__init__.py
+++ library/utils/pylib_loader/__init__.py
@@ -14,10 +14,10 @@ elif platform_name == 'Linux':
     from .unix import pylib
 elif platform_name == 'Darwin':
     try:
-        from macos_m1 import pylib
+        from .macos_m1 import pylib
         print('M1 binary imported')
     except ImportError:
-        from macos_x86 import pylib
+        from .macos_x86 import pylib
         print('X86 binary imported')
 else:
     raise UnsupportedSystem(f'System {platform_name} not suppported')
