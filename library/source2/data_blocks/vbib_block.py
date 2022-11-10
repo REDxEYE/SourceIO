63d1c95083c3e3ee8d1ed88a86b385ab5eb2c77c
REDxEYE <med45c@gmail.com>
2022-11-10 19:35:42 +0300

    Add missing R16G16_SNORM case

--- library/source2/data_blocks/vbib_block.py
+++ library/source2/data_blocks/vbib_block.py
@@ -256,6 +256,8 @@ class VertexAttribute:
             return np.uint8, (4,)
         elif self.format == DxgiFormat.R16G16_UNORM:
             return np.uint16, (2,)
+        elif self.format == DxgiFormat.R16G16_SNORM:
+            return np.int16, (2,)
         else:
             raise NotImplementedError(f"UNSUPPORTED DXGI format {self.format.name}")
 
