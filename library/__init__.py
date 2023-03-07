abb8e574f27898e40c9f3723681079a1c3592667
REDxEYE <med45c@gmail.com>
2023-03-07 14:46:19 +0300

    Add bforartists to list of recognized blender executables

--- library/__init__.py
+++ library/__init__.py
@@ -4,7 +4,8 @@ from pathlib import Path
 
 
 def running_in_blender():
-    return 'blender' in Path(sys.argv[0]).stem.lower()
+    exe_name = Path(sys.argv[0]).stem.lower()
+    return "blender" in exe_name or "bforartists" in exe_name
 
 
 def loaded_as_addon():
