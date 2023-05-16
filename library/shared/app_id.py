e8f38d8a5443c188e8ed372e6d314e309f83c1ae
REDxEYE <med45c@gmail.com>
2023-05-16 16:54:42 +0300

    Weird issue with python 3.11

--- library/shared/app_id.py
+++ library/shared/app_id.py
@@ -27,12 +27,12 @@ class SteamAppId(IntEnum):
         possible_member = cls._value2member_map_.get(value, None)
 
         if possible_member is None:
-            possible_member = cls._create_pseudo_member_(value)
+            possible_member = cls.create_pseudo_member(value)
 
         return possible_member
 
     @classmethod
-    def _create_pseudo_member_(cls, value):
+    def create_pseudo_member(cls, value):
         """
         Create a default-name member.
         """
