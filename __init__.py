58686ee0170280edc498c859c4956cfa127cc09d
REDxEYE <med45c@gmail.com>
2023-10-28 21:10:51 +0300

    Version bump

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ from .library import loaded_as_addon, running_in_blender
 bl_info = {
     "name": "SourceIO",
     "author": "RED_EYE, ShadelessFox, Syborg64",
-    "version": (5, 1, 0),
+    "version": (5, 2, 0),
     "blender": (3, 1, 0),
     "location": "File > Import-Export > SourceEngine assets",
     "description": "GoldSrc/Source1/Source2 Engine assets(.mdl, .bsp, .vmt, .vtf, .vmdl_c, .vwrld_c, .vtex_c)"
