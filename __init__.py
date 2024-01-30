bf3f7468fb365d44f4f969e07df855cc31a69080
REDxEYE <med45c@gmail.com>
2024-01-30 16:10:57 +0300

    Handle cases where plugin installed with -master and other prefixes

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
-from .library import loaded_as_addon, running_in_blender
+import sys
+from pathlib import Path
 
 bl_info = {
     "name": "SourceIO",
@@ -10,8 +11,19 @@ bl_info = {
                    "Notice that you cannot delete this addon via blender UI, remove it manually from addons folder",
     "category": "Import-Export"
 }
+
+if "SourceIO" not in sys.modules:
+    sys.modules['SourceIO'] = sys.modules[Path(__file__).parent.stem]
+
+import bpy
+
+if bpy.app.version >= (4, 0, 0) and bl_info["version"] <= (5, 2, 0):
+    print("SourceIO only support blender 4.X.X in latest development version")
+
+from SourceIO.library import loaded_as_addon, running_in_blender
+
 if running_in_blender() and loaded_as_addon():
-    from .blender_bindings.bindings import register, unregister
+    from SourceIO.blender_bindings.bindings import register, unregister
 
     if __name__ == "__main__":
         register()
