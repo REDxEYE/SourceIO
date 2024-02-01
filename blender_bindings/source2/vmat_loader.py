3caf52bfdd13506c307ce904aea32e3d3fd3a080
REDxEYE <med45c@gmail.com>
2024-02-01 14:02:12 +0300

    Fix source2 materials

--- blender_bindings/source2/vmat_loader.py
+++ blender_bindings/source2/vmat_loader.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+from ..utils.bpy_utils import find_or_create_material
 from ...blender_bindings.material_loader.material_loader import \
     Source2MaterialLoader
 from ...library.source2 import CompiledMaterialResource
@@ -7,4 +8,5 @@ from ...library.source2 import CompiledMaterialResource
 
 def load_material(material_resource: CompiledMaterialResource, material_path: Path, tinted: bool = False):
     source_material = Source2MaterialLoader(material_resource, material_path.stem, tinted)
-    source_material.create_material()
+    material = find_or_create_material(material_path.stem, material_path.as_posix())
+    source_material.create_material(material)
