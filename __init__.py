16fc3aec6f0d86364e56306c06bf02210b8a5fbf
REDxEYE <med45c@gmail.com>
2024-03-19 14:03:47 +0300

    version bump

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ from pathlib import Path
 bl_info = {
     "name": "SourceIO",
     "author": "RED_EYE, ShadelessFox, Syborg64",
-    "version": (5, 2, 0),
+    "version": (5, 3, 0),
     "blender": (3, 1, 0),
     "location": "File > Import-Export > SourceEngine assets",
     "description": "GoldSrc/Source1/Source2 Engine assets(.mdl, .bsp, .vmt, .vtf, .vmdl_c, .vwrld_c, .vtex_c)"
