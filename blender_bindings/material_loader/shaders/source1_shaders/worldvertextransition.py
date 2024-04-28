1f1669f1da8487b758284bf419d177b1684cb413
hisanimations <41131633+hisprofile@users.noreply.github.com>
2024-04-28 20:25:05 +0200

    Update worldvertextransition.py

--- blender_bindings/material_loader/shaders/source1_shaders/worldvertextransition.py
+++ blender_bindings/material_loader/shaders/source1_shaders/worldvertextransition.py
@@ -2,8 +2,10 @@ from ....utils.bpy_utils import is_blender_4
 from ...shader_base import Nodes
 from ..source1_shader_base import Source1ShaderBase
 
+from .detail import DetailSupportMixin
 
-class WorldVertexTransition(Source1ShaderBase):
+
+class WorldVertexTransition(DetailSupportMixin, Source1ShaderBase):
     SHADER = 'worldvertextransition'
 
     @property
@@ -11,6 +13,17 @@ class WorldVertexTransition(Source1ShaderBase):
         texture_path = self._vmt.get_string('$basetexture', None)
         if texture_path is not None:
             return self.load_texture_or_default(texture_path, (0.3, 0.0, 0.3, 1.0))
+        return None
+    
+    @property
+    def blendmodulatetexture(self):
+        texture_path = self._vmt.get_string('$blendmodulatetexture', None)
+        if texture_path is not None:
+            image = self.load_texture_or_default(texture_path, (0.3, 0.0, 0.3, 1.0))
+            image.colorspace_settings.is_data = True
+            image.colorspace_settings.name = 'Non-Color'
+            return image
+
         return None
 
     @property
@@ -25,6 +38,8 @@ class WorldVertexTransition(Source1ShaderBase):
         texture_path = self._vmt.get_string('$bumpmap', None)
         if texture_path is not None:
             image = self.load_texture_or_default(texture_path, (0.6, 0.0, 0.6, 1.0))
+            if self.ssbump:
+                image = self.convert_ssbump(image)
             image.colorspace_settings.is_data = True
             image.colorspace_settings.name = 'Non-Color'
             return image
@@ -35,8 +50,18 @@ class WorldVertexTransition(Source1ShaderBase):
         texture_path = self._vmt.get_string('$bumpmap2', None)
         if texture_path is not None:
             image = self.load_texture_or_default(texture_path, (0.6, 0.6, 0.0, 1.0))
+            if self.ssbump:
+                image = self.convert_ssbump(image)
             image.colorspace_settings.is_data = True
             image.colorspace_settings.name = 'Non-Color'
+            return image
+        return None
+    
+    @property
+    def detail(self):
+        texture_path = self._vmt.get_string('$detail', None)
+        if texture_path is not None:
+            image = self.load_texture_or_default(texture_path, (0.6, 0.6, 0.0, 1.0))
             return image
         return None
 
@@ -79,8 +104,32 @@ class WorldVertexTransition(Source1ShaderBase):
             vertex_color = self.create_node(Nodes.ShaderNodeVertexColor)
             
             color_mix = self.create_node(Nodes.ShaderNodeMixRGB)
-            self.connect_nodes(vertex_color.outputs['Color'], color_mix.inputs['Fac'])
+            if self.blendmodulatetexture != None:
+                SEPrgb = self.create_node(Nodes.ShaderNodeSeparateRGB)
+                sub = self.create_node(Nodes.ShaderNodeMath)
+                sub.operation = 'SUBTRACT'
+                add = self.create_node(Nodes.ShaderNodeMath)
+                add.operation = 'ADD'
+                maprange = self.create_node(Nodes.ShaderNodeMapRange)
+                maprange.interpolation_type = 'SMOOTHSTEP'
+
+                self.connect_nodes(SEPrgb.outputs[1], sub.inputs[0])
+                self.connect_nodes(SEPrgb.outputs[0], sub.inputs[1])
+                self.connect_nodes(SEPrgb.outputs[0], add.inputs[0])
+                self.connect_nodes(SEPrgb.outputs[1], add.inputs[1])
+                self.connect_nodes(sub.outputs[0], maprange.inputs[1])
+                self.connect_nodes(add.outputs[0], maprange.inputs[2])
+                self.connect_nodes(vertex_color.outputs[0], maprange.inputs[0])
+                self.connect_nodes(maprange.outputs[0], color_mix.inputs['Fac'])
+
+                self.create_and_connect_texture_node(self.blendmodulatetexture, SEPrgb.inputs[0], name='$blendmodulatedecal')
+
+                #self.create_texture_node(self.blendmodulatetexture, '$blendmodulatedecal')
+            else:
+                self.connect_nodes(vertex_color.outputs['Color'], color_mix.inputs['Fac'])
             color_mix.blend_type = 'MIX'
+
+            albedo = color_mix.outputs[0]
 
             self.create_and_connect_texture_node(basetexture,
                                                  color_mix.inputs['Color1'],
@@ -88,8 +137,11 @@ class WorldVertexTransition(Source1ShaderBase):
             self.create_and_connect_texture_node(basetexture2,
                                                  color_mix.inputs['Color2'],
                                                  name='$basetexture2')
-
-            self.connect_nodes(color_mix.outputs['Color'], shader.inputs['Base Color'])
+            
+            if self.detail:
+                albedo, detail = self.handle_detail(shader.inputs['Base Color'], albedo, uv_node=None)
+            else:
+                self.connect_nodes(color_mix.outputs['Color'], shader.inputs['Base Color'])
 
         if not self.phong:
             if is_blender_4():
