41f9605c1b1442d6a24093d760dc533f9b9ddafc
hisanimations <41131633+hisprofile@users.noreply.github.com>
2024-04-29 22:57:57 +0200

    Update source1_shader_base.py

--- blender_bindings/material_loader/shaders/source1_shader_base.py
+++ blender_bindings/material_loader/shaders/source1_shader_base.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 import bpy
 import numpy as np
+from mathutils import Vector
 
 from ...utils.texture_utils import check_texture_cache
 from ....library.shared.content_providers.content_manager import ContentManager
@@ -9,21 +10,33 @@ from ...source1.vtf import import_texture
 from ..shader_base import ShaderBase
 from ....library.source1.vmt import VMT
 
-
 class Source1ShaderBase(ShaderBase):
     def __init__(self, vmt):
         super().__init__()
         self.load_bvlg_nodes()
         self._vmt: VMT = vmt
         self.textures = {}
-
+    'r'.rsplit()
     def load_texture(self, texture_name: str, texture_path: Path):
         image = check_texture_cache(texture_path / texture_name)
         if image is not None:
             return image
-        if bpy.data.images.get(texture_name, False):
-            self.logger.debug(f'Using existing texture {texture_name}')
-            return bpy.data.images.get(texture_name)
+        
+        # ugly, but we don't want to reuse textures with the same name but different paths
+
+        #for image in bpy.data.images:
+            
+        #print((texture_path / texture_name).as_posix())
+
+        for image in bpy.data.images:
+            if (fp := image.get('full_path')) == None: continue
+            if fp == (texture_path / texture_name).as_posix().lower():
+                self.logger.debug(f'Using existing texture {texture_name}')
+                return image
+
+        #if bpy.data.images.get(texture_name, False):
+        #    self.logger.debug(f'Using existing texture {texture_name}')
+        #    return bpy.data.images.get(texture_name)
 
         content_manager = ContentManager()
         texture_file = content_manager.find_texture(texture_path / texture_name)
@@ -51,7 +64,7 @@ class Source1ShaderBase(ShaderBase):
         dots += 0.5
 
         buffer[:, :3] = np.clip(dots, 0, 1)
-        buffer[:, 1] = np.subtract(1, buffer[:, 1])
+        #buffer[:, 1] = np.subtract(1, buffer[:, 1])
 
         image.pixels.foreach_set(buffer.ravel())
         image.pack()
