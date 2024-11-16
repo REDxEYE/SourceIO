c2b16ef434cfb5b74a92628b4d230743d8328311
REDxEYE <med45c@gmail.com>
2024-11-16 21:45:20 +0300

    Version bump

--- __init__.py
+++ __init__.py
@@ -4,8 +4,8 @@ from pathlib import Path
 bl_info = {
     "name": "SourceIO",
     "author": "RED_EYE, ShadelessFox, Syborg64",
-    "version": (5, 4, 5),
-    "blender": (3, 6, 0),
+    "version": (5, 4, 6),
+    "blender": (4, 0, 0),
     "location": "File > Import > SourceEngine assets",
     "description": "GoldSrc/Source1/Source2 Engine assets(.mdl, .bsp, .vmt, .vtf, .vmdl_c, .vwrld_c, .vtex_c)"
                    "Notice that you cannot delete this addon via blender UI, remove it manually from addons folder",
@@ -20,8 +20,8 @@ from SourceIO.library import loaded_as_addon, running_in_blender
 if running_in_blender() and loaded_as_addon():
     import bpy
 
-    if bpy.app.version >= (4, 0, 0) and bl_info["version"] <= (5, 3, 0):
-        print("SourceIO only support blender 4.X.X in latest development version")
+    if bpy.app.version >= (4, 0, 0):
+        print("SourceIO only support blender 4.X.X")
 
     from SourceIO.blender_bindings.bindings import register, unregister
 
