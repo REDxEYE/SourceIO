fe03ba72c8b1a2cbf64fdab430100eaf5729e902
REDxEYE <med45c@gmail.com>
2025-10-14 20:23:25 +0300

    Actual version bump

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ from pathlib import Path
 bl_info = {
     "name": "SourceIO",
     "author": "RED_EYE, ShadelessFox, Syborg64",
-    "version": (5, 5, 0),
+    "version": (5, 5, 1),
     "blender": (4, 0, 0),
     "location": "File > Import > SourceEngine assets",
     "description": "GoldSrc/Source1/Source2 Engine assets(.mdl, .bsp, .vmt, .vtf, .vmdl_c, .vwrld_c, .vtex_c)"
@@ -21,7 +21,7 @@ from SourceIO.library import loaded_as_addon, running_in_blender
 if running_in_blender() and loaded_as_addon():
     import bpy
 
-    if bpy.app.version >= (4, 0, 0):
+    if bpy.app.version < (4, 0, 0):
         print("SourceIO only support blender 4.X.X")
 
     from SourceIO.blender_bindings.bindings import register, unregister
